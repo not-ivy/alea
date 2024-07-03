153b31e9c0409bf802e096be7d882d2d6ca77ea9
nora <nora@norain.icu>
2024-07-02 21:37:45 -0700

    add more benchmarks

--- mod.bench.ts
+++ mod.bench.ts
@@ -1,24 +1,27 @@
-import OldAlea, { OldMash } from "./mod.old.ts";
-import Alea, { Mash } from "./mod.ts";
-
-const newMash = new Mash();
-const oldMash = OldMash();
+import OldAlea from "./mod.old.ts";
+import Alea from "./mod.ts";
+import xorshift from "https://esm.sh/xorshift@1.2.0";
+import random from "https://esm.sh/random@4.1.0";
 
 const newAlea = new Alea();
 const oldAlea = OldAlea();
 
-Deno.bench("mash-old", () => {
-  oldMash(" ");
+Deno.bench("Math.random", () => {
+  Math.random();
 });
 
-Deno.bench("mash-new", () => {
-  newMash.mash(" ");
+Deno.bench("random", () => {
+  random.float();
+});
+
+Deno.bench("xorshift", () => {
+  xorshift.random();
 });
 
 Deno.bench("alea-old", () => {
   oldAlea();
 });
 
-Deno.bench("alea-new", () => {
+Deno.bench("alea-new", { baseline: true }, () => {
   newAlea.random();
 });
