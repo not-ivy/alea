bba99f357c72e27ce80f0af987ceb63a2355572e
nora <nora@norain.icu>
2024-07-05 11:32:53 -0700

    add tests for triangular and normal

--- mod.spec.ts
+++ mod.spec.ts
@@ -1,13 +1,14 @@
-import { assertEquals } from "jsr:@std/assert@^0.226.0";
+import { assert, assertEquals } from "jsr:@std/assert@^0.226.0";
 import Alea, { Mash } from "./mod.ts";
 import OldAlea, { OldMash } from "./mod.old.ts";
 
 const newMash = new Mash();
 const oldMash = OldMash();
 
-const newAlea = new Alea({ seed: "meow" });
 //@ts-ignore: typescript is being silly
-const oldAlea = new OldAlea("meow");
+const aleaFactory = () => [new Alea({ seed: "meow" }), new OldAlea("meow")];
+// deno-lint-ignore no-explicit-any
+let [newAlea, oldAlea] = aleaFactory() as [Alea, any];
 
 Deno.test("mash", () => {
   let ok = 0;
@@ -22,9 +23,28 @@ Deno.test("alea", async (t) => {
     assertEquals(newAlea.seed, "meow");
   });
 
+  await t.step("normal", () => {
+    const normal = [] as number[];
+    for (let i = 0; i < 1e3; i++) {
+      normal.push(newAlea.normal());
+    }
+
+    assert(normal.every((n) => n >= -3 && n <= 3)); // we could probably write better tests for normal and triangular
+  });
+
+  await t.step("triangular", () => {
+    const triangular = [] as number[];
+    for (let i = 0; i < 1e3; i++) {
+      triangular.push(newAlea.triangular(0, 3, 0));
+    }
+
+    assert(triangular.every((n) => n >= 0 && n <= 3));
+  });
+
   let ok = 0;
+  [newAlea, oldAlea] = aleaFactory();
   await t.step("random", () => {
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 1e3; i++) {
       if (newAlea.random() === oldAlea()) ok += 1;
     }
     assertEquals(ok, 1000);
@@ -32,7 +52,7 @@ Deno.test("alea", async (t) => {
 
   ok = 0;
   await t.step("fract53", () => {
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 1e3; i++) {
       if (newAlea.fract53() === oldAlea.fract53()) ok += 1;
     }
     assertEquals(ok, 1000);
@@ -40,7 +60,7 @@ Deno.test("alea", async (t) => {
 
   ok = 0;
   await t.step("uint32", () => {
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 1e3; i++) {
       if (newAlea.uint32() === oldAlea.uint32()) ok += 1;
     }
     assertEquals(ok, 1000);
